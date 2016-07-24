69cb34d1ede4d67d1db1a9abfe1cd67e52e47e93
nizamovedik <nizamovedik@yandex.ru>
2016-07-24 21:19:19 +0500

    class vert line

---
+++ Snake/VertLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class VertLine
+    {
+        List<Point> pList;
+
+        public VertLine(int x, int yTop, int yBot, char sym)
+        {
+            pList = new List<Point>();
+            for (int i = yTop; i <= yBot; i++)
+            {
+                Point p = new Point(x, i, sym);
+                pList.Add(p);
+            }
+        }
+        public void DrowVertLine()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
