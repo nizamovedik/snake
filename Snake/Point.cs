353bb162b74478cec8a14a10943cb5192b009591
nizamovedik <nizamovedik@yandex.ru>
2016-07-24 19:42:09 +0500

    конструктор

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -7,9 +7,17 @@ namespace Snake
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
+        private int x;
+        private int y;
+        private char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
