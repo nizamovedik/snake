dc1d476f91189ea318890830bf5877b40b4f6af0
nizamovedik <nizamovedik@yandex.ru>
2016-07-24 21:19:03 +0500

    class horizont line

---
+++ Snake/HorizLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class HorizLine
+    {
+        List<Point> pList;
+
+        public HorizLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, sym);
+                pList.Add( p );
+            }
+        }
+        public void DrowHorizLine()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
